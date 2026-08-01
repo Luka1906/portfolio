9a78b6a5194b9bbdadfee457cc40d579affc001a
Luka Matovic <lukamatovic1994@gmail.com>
2026-08-01 01:32:44 -0500

    Initialize portfolio project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <main className='min-h-screen bg-slate-950'> 
+      <h1 className='text-6xl font-bold text-white'>Luka Matovic</h1>
+    </main>
+  )
+}
+
+export default App
